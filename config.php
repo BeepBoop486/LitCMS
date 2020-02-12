1037650fd9073626549fac3a0cb4e5f490aa3324
BeepBoop486 <sustix123@gmail.com>
2020-02-12 16:40:19 -0500

    Added some styles, scripts and loaded'em

--- config.php
+++ config.php
@@ -1,8 +1,21 @@
 <?php 
 
+	//SITE CONFIG
 	$GLOBALS['SITE_NAME'] = "LitCMS";
 	$GLOBALS['SITE_DESC'] = "A lightweight and opensource CMS";
 	$GLOBALS['SITE_TAGS'] = "lightweight, cms, free, opensource, gnu, gpl, php, html, css, github, git, bootstrap";
 	$GLOBALS['SITE_AUTH'] = "BeepBoop486";
+
+	//SITE DIRS CONFIG
+	$GLOBALS['CSS_DIR'] = "/css/";
+	$GLOBALS['JS_DIR'] = "/js/";
+
+	//SITE STYLES
+	$styles[] = $GLOBALS["CSS_DIR"] . "bootstrap.min.css";
+
+	//SITE SCRIPTS
+	$scripts[] = $GLOBALS["JS_DIR"] . "jquery-3.4.1.min.js";
+	$scripts[] = $GLOBALS["JS_DIR"] . "bootstrap.min.js";
+	$scripts[] = $GLOBALS["JS_DIR"] . "bootstrap.bundle.js";
 
 ?>
--- inc/header.php
+++ inc/header.php
@@ -9,5 +9,16 @@
 	<meta name="keywords" content="<?php echo $GLOBALS['SITE_TAGS']; ?>">
 	<meta name="author" content="<?php echo $GLOBALS['SITE_AUTH']; ?>">
 
+	<?php 
+
+		for($i = 0; $i < count($styles); $i++) {
+			echo '<link rel="stylesheet" type="text/css" href="'.$styles[$i].'">';
+		}
+		for($i = 0; $i < count($scripts); $i++) {
+		echo '<script type="text/javascript" src="'.$scripts[$i].'"></script>';
+		}
+
+	 ?>
+
 </head>
 <body>
