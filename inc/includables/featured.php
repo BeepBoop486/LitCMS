dcdd8ebc11c089a0c80c2e99cbeabbb22417baef
sarah <sustix123@gmail.com>
2020-03-05 21:51:49 +0000

    This bug finally fixed

--- inc/includables/featured.php
+++ inc/includables/featured.php
@@ -10,55 +10,50 @@
 				$stmt->bind_param("i", $feat);
 				$stmt->execute();
 
-				$stmt->store_result();
-				$rows = $stmt->num_rows;
-
 				$stmt->bind_result($pid, $pname, $pcnt, $pupl, $pthumb, $pdate, $ptags, $pcat, $featured);
 				$stmt->fetch();
 
-					//TODO: Go to an entry
-
-				if($rows > 0) {
 					echo '
 						<div class="featured__column featured__column--big sep">
 							<div class="entry" style="background-image: url('.$pthumb.')">
 								<div class="entry__content">
-								<span class="entry__category"><a href="#">'.$pcat.'</a></span>
-								<h1><a href="/p/'.$pid.'">'.$pname.'</a></h1>
-								<div class="entry__info">
-									<a href="#" class="entry__profile-pic">
-										<img class="avatar" src="/images/default-pp.png">
-									</a>
-									<ul class="entry__meta">
-										<li><a href="#0">'.$pupl.'</a></li>
-										<li>'.$pdate.'</li>
-									</ul>
-								</div>
+									<span class="entry__category"><a href="#">'.$pcat.'</a></span>
+									<h1><a href="/p/'.$pid.'">'.$pname.'</a></h1>
+									<div class="entry__info">
+										<a href="#" class="entry__profile-pic">
+											<img class="avatar" src="/images/default-pp.png">
+										</a>
+										<ul class="entry__meta">
+											<li><a href="#0">'.$pupl.'</a></li>
+											<li>'.$pdate.'</li>
+										</ul>
+									</div>
 								</div>
 							</div>
 						</div>
 					';
-				}
 
 				$stmt->close();
 
-				$stmt = $conn->prepare("SELECT * FROM posts ORDER BY id DESC LIMIT 2");
-				$stmt->execute();
-				while($stmt->fetch()) {
-					$stmt->bind_result($pid, $pname, $pcnt, $pupl, $pthumb, $pdate, $ptags, $pcat, $featured);
+				$feat = 0;
+				$stmt1 = $conn->prepare("SELECT * FROM posts WHERE is_featured=? ORDER BY id DESC LIMIT 2");
+				$stmt1->bind_param("i", $feat);
+				$stmt1->execute();
+				$stmt1->bind_result($ppid, $ppname, $ppcnt, $ppupl, $ppthumb, $ppdate, $pptags, $ppcat, $pfeatured);
+				while($stmt1->fetch()) {
 					echo '
 						<div class="featured__column featured__column--small sephalf">
-							<div class="entry" style="background-image: url('.$pthumb.')">
+							<div class="entry" style="background-image: url('.$ppthumb.')">
 								<div class="entry__content">
-								<span class="entry__category"><a href="#">'.$pcat.'</a></span>
-								<h1><a href="/p/'.$pid.'">'.$pname.'</a></h1>
+								<span class="entry__category"><a href="#">'.$ppcat.'</a></span>
+								<h1><a href="/p/'.$ppid.'">'.$ppname.'</a></h1>
 								<div class="entry__info">
 									<a href="#" class="entry__profile-pic">
 										<img class="avatar" src="/images/default-pp.png">
 									</a>
 									<ul class="entry__meta">
-										<li><a href="#0">'.$pupl.'</a></li>
-										<li>'.$pdate.'</li>
+										<li><a href="#0">'.$ppupl.'</a></li>
+										<li>'.$ppdate.'</li>
 									</ul>
 								</div>
 								</div>
@@ -67,7 +62,7 @@
 					';
 				}
 
-				$stmt->close();
+				$stmt1->close();
 			?>
 
 		</div>
