3ff1e19c8ad6d3c835a7d255353e1f434232f1b0
BeepBoop486 <sustix123@gmail.com>
2020-03-06 09:55:12 -0500

    Done site config editin'

--- adminp/modif/site.php
+++ adminp/modif/site.php
@@ -2,6 +2,18 @@
 
 	include '../inc/header.php';
 
+	$stmt = $conn->prepare("SELECT * FROM config LIMIT 1");
+	$stmt->execute();
+	$stmt->store_result();
+	$alr = $stmt->num_rows;
+	if ($alr > 0) {
+		$stmt->bind_result($n,$d,$t,$a,$f,$re,$sf,$st,$si,$sp);
+		$stmt->fetch();
+	} else {
+		$n = ""; $d = ""; $t = ""; $a = ""; $f = 0; $re = 0; $sf = ""; $st = ""; $si = ""; $sp = "";
+	}
+	$stmt->close();
+
 	if (isset($_POST["submit"])) {
 		$sitename = $_POST["sitename"];
 		$sitetags = $_POST["sitetags"];
@@ -15,12 +27,16 @@
 		$pint = $_POST["pint"];
 
 		if ($sitename && $sitetags && $sitedescr && $siteauth && $face && $inst && $twit && $pint) {
-			$stmt = $conn->prepare("INSERT INTO config(SITE_NAME,SITE_DESC,SITE_TAGS,SITE_AUTH,FEATURED,REGISTRATION_ENABLED,SOCIAL_FACEBOOK,SOCIAL_TWITTER,SOCIAL_INSTAGRAM,SOCIAL_PINTEREST) VALUES(?,?,?,?,?,?,?,?,?,?)");
-			$stmt->bind_param("ssssiissss", $sitename, $sitedescr, $sitetags, $siteauth, $sitefeat, $signup, $face, $twit, $inst, $pint);
-			if ($stmt->execute()) {
-				echo "<script>window.location.href='/adminp/siteSets'</script>";
+			if($alr == 0) {
+				$stmt = $conn->prepare("INSERT INTO config(SITE_NAME,SITE_DESC,SITE_TAGS,SITE_AUTH,FEATURED,REGISTRATION_ENABLED,SOCIAL_FACEBOOK,SOCIAL_TWITTER,SOCIAL_INSTAGRAM,SOCIAL_PINTEREST) VALUES(?,?,?,?,?,?,?,?,?,?)");
+				$stmt->bind_param("ssssiissss", $sitename, $sitedescr, $sitetags, $siteauth, $sitefeat, $signup, $face, $twit, $inst, $pint);
+				if ($stmt->execute()) {
+					echo "<script>window.location.href='/adminp/siteSets'</script>";
+				}
+				$stmt->close();
+			} else {
+				//TODO: Update
 			}
-			$stmt->close();
 		} else {
 			echo "You must to fill all of the fields";
 		}
@@ -33,52 +49,52 @@
 		
 		<div class="form-group col-md-8">
 			<label>Site name:</label>
-			<input type="text" name="sitename" class="form-control">
+			<input type="text" name="sitename" class="form-control" value="<?php echo $n; ?>">
 		</div>
 
 		<div class="form-group col-md-4">
 			<label>Site tags:</label>
-			<input type="text" name="sitetags" class="form-control">
+			<input type="text" name="sitetags" class="form-control" value="<?php echo $t; ?>">
 		</div>
 
 		<div class="form-group col-md-12">
 			<label>Site description:</label>
-			<textarea rows="8" class="form-control" name="sitedescr"></textarea>
+			<textarea rows="8" class="form-control" name="sitedescr"><?php echo $d; ?></textarea>
 		</div>
 
 		<div class="form-group col-md-4">
 			<label>Site author:</label>
-			<input type="text" name="siteauth" class="form-control">
+			<input type="text" name="siteauth" class="form-control" value="<?php echo $a; ?>">
 		</div>
 
 		<div class="form-group col-md-2">
 			<label>Show featured:</label>
-			<input type="text" name="sitefeat" class="form-control" disabled="" value="1">
+			<input type="text" name="sitefeat" class="form-control" disabled="" value="<?php echo $f; ?>">
 		</div>
 
 		<div class="form-group col-md-4">
 			<label>Enabled sign-up:</label>
-			<input type="number" name="signup" class="form-control" value="0">
+			<input type="number" name="signup" class="form-control" value="<?php echo $re; ?>">
 		</div>
 
 		<div class="form-group col-md-3">
 			<label>Facebook:</label>
-			<input type="text" name="face" class="form-control">
+			<input type="text" name="face" class="form-control" value="<?php echo $sf; ?>">
 		</div>
 
 		<div class="form-group col-md-3">
 			<label>Twitter:</label>
-			<input type="text" name="twit" class="form-control">
+			<input type="text" name="twit" class="form-control" value="<?php echo $st; ?>">
 		</div>
 
 		<div class="form-group col-md-3">
 			<label>Instagram:</label>
-			<input type="text" name="inst" class="form-control">
+			<input type="text" name="inst" class="form-control" value="<?php echo $si; ?>">
 		</div>
 
 		<div class="form-group col-md-3">
 			<label>Pinterest:</label>
-			<input type="text" name="pint" class="form-control">
+			<input type="text" name="pint" class="form-control" value="<?php echo $sp; ?>">
 		</div>
 
 		<div class="col-md-12">
